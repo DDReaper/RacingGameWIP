78836cf258b518d228328e6d3559dddefad7f4ab
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2013-08-06 21:44:19 +0100

    Initial Checkin = prior CartBlanche fixes

---
+++ RacingGame/Game/Storage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Storage;
+using System;
+
+namespace RacingGame
+{
+    public class Storage
+    {
+        public static StorageContainer OpenContainer(StorageDevice storageDevice, string saveGameName)
+        {
+            IAsyncResult result = storageDevice.BeginOpenContainer(saveGameName, null, null);
+
+            // Wait for the WaitHandle to become signaled.
+            result.AsyncWaitHandle.WaitOne();
+
+            StorageContainer container = storageDevice.EndOpenContainer(result);
+
+            // Close the wait handle.
+            result.AsyncWaitHandle.Close();
+
+            return container;
+        }
+    }
+}
